3f7be38bc858f721b14b8f558683c2817ae2cc30
ivan_chung <ivan_chung@realtek.com>
2024-02-15 19:28:53 +0800

    update keymaps

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -51,11 +51,3 @@ require("mason-lspconfig").setup_handlers({
   end
 })
 
--- Customized on_attach function
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
