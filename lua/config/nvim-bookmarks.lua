6430797d1e057f8cca5018cb01e26fe7a6a0927e
ivan_chung <ivan_chung@realtek.com>
2024-03-08 20:53:06 +0800

    add bookmark plugin config

---
+++ lua/config/nvim-bookmarks.lua
@@ -0,0 +1,13 @@
+require('bookmarks').setup {
+  -- sign_priority = 8,  --set bookmark sign priority to cover other sign
+  save_file = vim.fn.expand "$HOME/.bookmarks", -- bookmarks save file path
+  keywords =  {
+    ["@t"] = "☑️ ", -- mark annotation startswith @t ,signs this icon as `Todo`
+    ["@w"] = "⚠️ ", -- mark annotation startswith @w ,signs this icon as `Warn`
+    ["@f"] = "⛏ ", -- mark annotation startswith @f ,signs this icon as `Fix`
+    ["@n"] = "󰈙 ", -- mark annotation startswith @n ,signs this icon as `Note`
+  },
+}
+
+require('telescope').load_extension('bookmarks')
+
