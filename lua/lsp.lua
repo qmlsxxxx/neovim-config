3a6c7377f1b3f7f3773acb4319420d747361fea3
ivan_chung <ivan_chung@realtek.com>
2026-01-15 19:23:39 +0800

    update lsp-config usage

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -14,58 +14,21 @@ require('mason-lspconfig').setup({
     automatic_enable = false,
 })
 
-local function clangd_setup()
-    local lspconfig = require('lspconfig')
+vim.lsp.config('clangd', {
+  cmd = { 
+	  'clangd', 
+      '--background-index',
+	  '--suggest-missing-includes',
+	  '--clang-tidy', 
+	  '--header-insertion=iwyu',
+      '--completion-style=detailed',
+      '--enable-config',
+      '--j=4'
+  },
+  
+  filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
+  root_markers = { 'compile_commands.json', '.git' },
+})
 
-    
-    lspconfig.clangd.setup {
-        -- Clangd 特定配置
-        settings = {
-            clangd = {
-                -- 配置选项
-                fallbackFlags = {
-                    "-std=c++17",  -- 默认 C++17 标准
-                    "-Wall",       -- 启用常规警告
-                    "-Wextra"      -- 启用额外警告
-                },
-                
-                -- 建议配置
-                suggests = {
-                    autoImport = true,
-                    autoInclude = true
-                }
-            }
-        },
-        
-        -- 命令行参数
-        cmd = {
-            "clangd",
-            "--background-index",           -- 后台索引
-            "--suggest-missing-includes",   -- 建议缺失的头文件
-            "--clang-tidy",                 -- 启用 clang-tidy
-            "--header-insertion=iwyu",      -- 智能头文件插入
-            "--completion-style=detailed",  -- 详细补全
-            "--enable-config",              -- 启用本地配置文件
-            "--j=4"
-        },
-        
-        -- 根据项目类型动态配置
-        root_dir = function(fname)
-            local root_files = {
-                '.clangd',             -- Clangd 配置文件
-                '.clang-format',       -- Clang 格式配置
-                'compile_commands.json',
-                'compile_flags.txt',
-                'configure.ac',        -- Autotools 配置
-                'CMakeLists.txt',      -- CMake 项目
-                '.git'                 -- Git 仓库
-            }
-            
-            return lspconfig.util.root_pattern(unpack(root_files))(fname) 
-                or lspconfig.util.find_git_ancestor(fname)
-        end
-    }
-end
+vim.lsp.enable('clangd')
 
--- 初始化 Clangd 配置
-clangd_setup()
